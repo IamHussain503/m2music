1673828cd945cb7b7b991b7e2409efb18f751b4e
arain60gb <zainali5960@gmail.com>
2024-12-14 14:44:34 +0500

    change ddpm

--- MMGen_train/modules/latent_diffusion/ddpm.py
+++ MMGen_train/modules/latent_diffusion/ddpm.py
@@ -1272,9 +1272,9 @@ class LatentDiffusion(DDPM):
                     cond_dict[cond_model_key] = c
         
         # change the melody_npy and melody.faiss to the local path
-        melody_npy = np.load("/root/m2music/clm_data/embeddings/melody_embeddings.npy")
+        melody_npy = np.load("/root/backup_m2music/clm_data/embeddings/melody_embeddings.npy")
         melody_builder = FaissDatasetBuilder(melody_npy)
-        melody_builder.load_index("/root/m2music/clm_data/faiss/audio_2_melody_hnsw.faiss")
+        melody_builder.load_index("/root/backup_m2music/clm_data/faiss/audio_2_melody_hnsw.faiss")
         # change the melody_npy and melody.faiss to the local path
         
         query = cond_dict['film_clap_cond1']
