58dcf92e9c40b06e9532a60de945146fefe3db1f
arain60gb <zainali5960@gmail.com>
2024-12-14 14:31:34 +0500

    remove comments

--- MMGen_train/modules/clmp/training/data.py
+++ MMGen_train/modules/clmp/training/data.py
@@ -1069,27 +1069,6 @@ def get_toy_dataset(args, model_cfg, is_train):
 
     return DataInfo(dataloader, sampler)
 
-
-# def get_dataset_fn(data_path, dataset_type):
-#     if dataset_type == "webdataset":
-#         return get_wds_dataset
-#     elif dataset_type == "csv":
-#         return get_csv_dataset
-#     elif dataset_type == "auto":
-#         ext = data_path.split(".")[-1]
-#         if ext in ["csv", "tsv"]:
-#             return get_csv_dataset
-#         elif ext in ["tar"]:
-#             return get_wds_dataset
-#         else:
-#             raise ValueError(
-#                 f"Tried to figure out dataset type, but failed for extention {ext}."
-#             )
-#     elif dataset_type == "toy":
-#         return get_toy_dataset
-#     else:
-#         raise ValueError(f"Unsupported dataset type: {dataset_type}")
-
 def get_dataset_fn(data_path, dataset_type):
     try:
         print(f"get_dataset_fn called with:")
